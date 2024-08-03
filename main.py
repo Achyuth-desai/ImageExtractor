8e26667f5cb0ea9463fe005b785cef0a6bc9cca6
Achyuth-desai <desai.achyutha2000@gmail.com>
2024-08-03 11:16:14 +0530

    Folder name fix

--- main.py
+++ main.py
@@ -5,12 +5,9 @@ from superRes import super_res
 video_files = ['CD1.mp4', 'CD2.mp4', 'CD3.mp4']
 
 for video_file in video_files:
-    frame_extract_folder = 'Extracts'
-    superRes_extract_folder = 'Improved'
-    output_folder = 'Images'
-    frame_extract_folder += '_' + video_file
-    superRes_extract_folder += '_' + video_file
-    output_folder += '_' + video_file
+    frame_extract_folder += 'Extracts_' + video_file
+    superRes_extract_folder += 'Improved_' + video_file
+    output_folder += 'Images_' + video_file
     extract(video_file=video_file, output_folder=frame_extract_folder)
     print('EXTRACTION OF {} FILE IS COMPLETE. NOW RUNNING SUPER_RES TO IMPROVE THE RESOLUTION USING EDSR.'.format(video_file))
     super_res(image_folder=frame_extract_folder, output_folder=superRes_extract_folder)
